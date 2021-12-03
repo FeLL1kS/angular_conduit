2b321f0371e4be9f89acc0788f17349d859705ea
FeLL1kS <oleg5420@yandex.ru>
2021-12-03 22:50:00 +0300

    article-editor-tests

--- src/app/article-editor/article-editor.resolver.spec.ts
+++ src/app/article-editor/article-editor.resolver.spec.ts
@@ -1,25 +1,43 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 import { ArticleEditorResolver } from './article-editor.resolver';
 import { ArticleEditorService } from './article-editor.service';
 
 describe('ArticleEditorResolver', () => {
   let resolver: ArticleEditorResolver;
-  const articleEditorServiceStub = {
-    loadArticle: () => {},
-  };
+  let route: ActivatedRouteSnapshot;
+  let articleEditorServiceSpy: jasmine.SpyObj<ArticleEditorService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('ArticleEditorService', ['loadArticle']);
     TestBed.configureTestingModule({
       providers: [
         ArticleEditorResolver,
-        { provide: ArticleEditorService, useValue: articleEditorServiceStub },
+        { provide: ArticleEditorService, useValue: spy },
       ],
     });
     resolver = TestBed.inject(ArticleEditorResolver);
+    articleEditorServiceSpy = TestBed.inject(ArticleEditorService) as jasmine.SpyObj<ArticleEditorService>;
+    route = new ActivatedRouteSnapshot();
+    route.params = {
+      slug: 'slug'
+    }
   });
 
   it('should be created', () => {
     expect(resolver).toBeTruthy();
   });
+
+  it('articleEditorResolver should resolve', () => {
+    resolver.resolve(route).subscribe(res => {
+      expect(res).toBe(true);
+    });
+  })
+
+  it('loadArticles from ArticleEditorService should be called once', () => {
+    resolver.resolve(route).subscribe(res => {
+      expect(articleEditorServiceSpy.loadArticle.calls.count()).toBe(1);
+    });
+  })
 });
--- src/app/article-editor/article-editor/article-editor.component.spec.ts
+++ src/app/article-editor/article-editor/article-editor.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormGroup } from '@angular/forms';
-import { Observable, of } from 'rxjs';
-import { ArticleForm } from '../article-editor.interface';
+import { of, Subscription } from 'rxjs';
 import { ArticleEditorService } from '../article-editor.service';
 
 import { ArticleEditorComponent } from './article-editor.component';
@@ -9,18 +7,30 @@ import { ArticleEditorComponent } from './article-editor.component';
 describe('ArticleEditorComponent', () => {
   let component: ArticleEditorComponent;
   let fixture: ComponentFixture<ArticleEditorComponent>;
-  class articleEditorServiceStub {
-    form: FormGroup = new FormGroup({});
-    data$ = of({});
-  }
+  let articleEditorServiceSpy: jasmine.SpyObj<ArticleEditorService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('ArticleEditorService', 
+      ['createArticle', 'updateArticle', 'ngOnDestroy'], 
+      {
+        data$: of({
+          slug: '',
+          title: '',
+          description: '',
+          body: '',
+          tagList: ''
+        })
+      }
+    )
+
     await TestBed.configureTestingModule({
       declarations: [ArticleEditorComponent],
       providers: [
-        { provide: ArticleEditorService, useClass: articleEditorServiceStub },
+        { provide: ArticleEditorService, useValue: spy },
       ],
     }).compileComponents();
+
+    articleEditorServiceSpy = TestBed.inject(ArticleEditorService) as jasmine.SpyObj<ArticleEditorService>
   });
 
   beforeEach(() => {
@@ -32,4 +42,42 @@ describe('ArticleEditorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('form control getters', () => {
+    it('should return title control', () => {
+      expect(component.form.controls['title']).toBeTruthy();
+    })
+    
+    it('should return description control', () => {
+      expect(component.form.controls['description']).toBeTruthy();
+    })
+
+    it('should return body control', () => {
+      expect(component.form.controls['body']).toBeTruthy();
+    })
+
+    it('should return tagList control', () => {
+      expect(component.form.controls['tagList']).toBeTruthy();
+    })
+  })
+  
+  it('createArticle should only call createArticle from the service once', () => {
+    component.createArticle();
+
+    expect(articleEditorServiceSpy.createArticle.calls.count()).toBe(1);
+  })
+
+  it('updateArticle should only call updateArticle from the service once', () => {
+    component.updateArticle('');
+
+    expect(articleEditorServiceSpy.updateArticle.calls.count()).toBe(1);
+  })
+
+  it('ngOnDestroy should call unsubsribe', () => {
+    spyOn(Subscription.prototype, 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(Subscription.prototype.unsubscribe).toHaveBeenCalledTimes(1);
+  })
 });
